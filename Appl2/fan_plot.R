e5cfca99dea2126dac53f8d2f0d7c9bd23462719
Sebastian Meyer <seb.meyer@fau.de>
2020-08-13 16:38:59 +0200

    whitespace

--- Appl2/fan_plot.R
+++ Appl2/fan_plot.R
@@ -19,38 +19,38 @@ data[, PIT := pbeta(q = data_set.weighted_ili_org,
 
 for(r in 1:10){
   # Fan plot for region r
-  quantilesm <- result4$quantile_matrix[result4$result$data_set.region == 
+  quantilesm <- result4$quantile_matrix[result4$result$data_set.region ==
                                           paste("Region",r),]
   quantilesm <- as.matrix(quantilesm)
-  ym <- result4$result$data_set.weighted_ili_org[result4$result$data_set.region == 
+  ym <- result4$result$data_set.weighted_ili_org[result4$result$data_set.region ==
                                                    paste("Region",r)]
-  LSm <- -result4$result$log_score[result4$result$data_set.region == 
+  LSm <- -result4$result$log_score[result4$result$data_set.region ==
                                      paste("Region",r)]
-  DSSm <- result4$result$DS_score[result4$result$data_set.region == 
+  DSSm <- result4$result$DS_score[result4$result$data_set.region ==
                                     paste("Region",r)]
-  meanm <- result4$result$pt_pred[result4$result$data_set.region == 
+  meanm <- result4$result$pt_pred[result4$result$data_set.region ==
                                     paste("Region",r)]
-  upper_ylim <- max(quantilesm) 
+  upper_ylim <- max(quantilesm)
   ylimi <- c(0, upper_ylim)
-  upper_score <- max(LSm,DSSm) 
+  upper_score <- max(LSm,DSSm)
   lower_score <- min(LSm,DSSm)
   probs <- (1:99)/100
-  
+
   wILI.sts <- sts(observed = ym,
-                  epoch = result4$result$data_set.time[result4$result$data_set.region == 
-                                                         paste("Region",r)], 
+                  epoch = result4$result$data_set.time[result4$result$data_set.region ==
+                                                         paste("Region",r)],
                   epochAsDate = TRUE)
-  
+
   pdf(file = here::here(paste0("./Plots/Fan/mBeta_Region", r,".pdf")),
       width = width,
       height = height)
-  
+
   par(mar = c(1.5,2,0,0)+.5, cex.lab = 0.8, las = 1, mgp = c(1.5,0.5,0),
       lab = c(5,5,7), cex.axis = 0.8)
-  
+
   osaplot(
     quantiles = quantilesm * 100, probs = 1:99/100,
-    observed = ym * 100, 
+    observed = ym * 100,
     means = meanm * 100,
     scores = cbind(LS = LSm,
                    DSS = DSSm),
@@ -58,7 +58,7 @@ for(r in 1:10){
     ylim = ylimi * 100,
     ylab = "wILI (%)",
     main = "",
-    # cex.lab=0.6,  
+    # cex.lab=0.6,
     # cex.main=0.6, cex.sub=0.6, cex.axis=0.6,
     fan.args = list(ln = c(0.1,0.9), rlab = NULL),
     observed.args = list(cex = 0.2),
@@ -66,14 +66,14 @@ for(r in 1:10){
                        yaxs = "i",
                        xaxt = "n",
                        yaxt = "n",
-                       col = gray(c(0.5, 0)), 
+                       col = gray(c(0.5, 0)),
                        panel.first = {
                          axis(2, c(-10,0,10), lwd = 0, lwd.ticks = 1)
                          # abline(h = c(15, 30), col = "grey92",
                          #        v = match(2016:2019, strftime(epoch(wILI.sts), "%Y")))
                          # box()
                        }),
-    legend.args = if(r == 1) list(cex = 0.4, 
+    legend.args = if(r == 1) list(cex = 0.4,
                        inset = c(0.04,0)),
     key.args = if(r == 1) list(space = 1,
                      start = 198,
@@ -86,14 +86,14 @@ for(r in 1:10){
                     xaxis.labelFreq = list("%Y"=atMedian),
                     xaxis.labelFormat="%Y")
   dev.off()
-  
-  
+
+
   pdf(file = here::here(paste0("./Plots/Fan/PIT_Region", r,".pdf")),
       width = 1.94,
       height = 1.83)
-  par(mar = c(1.5,2,0,0)+.5, cex.lab = 0.8, las = 1, 
+  par(mar = c(1.5,2,0,0)+.5, cex.lab = 0.8, las = 1,
       cex.axis = 0.8, mgp = c(1.5,0.5,0))
-  hist(data[data_set.region == paste("Region",r),]$PIT, 
+  hist(data[data_set.region == paste("Region",r),]$PIT,
        freq = FALSE,
        ylab = "Relative Frequency",
        xlab = "",
@@ -115,7 +115,7 @@ data$season <- ifelse(
 ## Season week column: week number within season
 season_list <- unique(data.frame(season = data$season, time_index = data$time_index))
 data$season_week <- rep(sapply(seq_len(nrow(season_list)), function(row_ind) {
-  sum(season_list$season == season_list$season[row_ind] & 
+  sum(season_list$season == season_list$season[row_ind] &
         season_list$time_index <= season_list$time_index[row_ind])
 }), each = 10)
 
@@ -126,9 +126,9 @@ data$InSeason <- data$week %in% c(1:20, 40:53)
 # remove season 1997/1998 and season 2019/2020 because they are incomplete.
 data <- data[ !( season %in% c("1997/1998", "2019/2020")),]
 # take season 2014/2015, 2015/2016, 2016/2017 and 2017/2018 as test period.
-data[, train := !(season %in% c( "2015/2016", 
-                                 "2016/2017", 
-                                 "2017/2018", 
+data[, train := !(season %in% c( "2015/2016",
+                                 "2016/2017",
+                                 "2017/2018",
                                  "2018/2019"))]
 
 data[, group := ifelse(time <= "2010-07-25" & time >= "2008-07-27", 2,
@@ -151,29 +151,29 @@ p1 <- ggplot(data = data[data$region %in% paste("Region", 1:5),], aes(x=time, y=
   xlab("Year") +
   ylab("wILI (%)") +
   ylim(0, 12.5) +
-  geom_vline(xintercept = data$time[which(data$train == FALSE)[1]], 
+  geom_vline(xintercept = data$time[which(data$train == FALSE)[1]],
              linetype="dashed", color = "black", size = 1)  +
   theme_bw() +
   theme(legend.position = "none")
 
 p1 + facet_grid(rows = vars(region),  scales="free_x" , as.table=TRUE) + xlab("Year") +
-  ylab("wILI (%)") 
+  ylab("wILI (%)")
 dev.off()
 
 pdf(file = here::here("./Plots/wILI2.pdf"), width = 8, height = 9)
 data$group <- as.factor(data$group)
-p1 <- ggplot(data = data[data$region %in% paste("Region", 6:10),], 
+p1 <- ggplot(data = data[data$region %in% paste("Region", 6:10),],
              aes(x=time, y=weighted_ili, group = group)) +
   geom_line(aes(color = group)) +
   scale_color_manual(values=c("black", "grey", "black")) +
   xlab("Year") +
   ylab("wILI (%)") +
   ylim(0, 12.5) +
-  geom_vline(xintercept = data$time[which(data$train == FALSE)[1]], 
+  geom_vline(xintercept = data$time[which(data$train == FALSE)[1]],
              linetype="dashed", color = "black", size = 1)  +
   theme_bw() +
   theme(legend.position = "none")
 
 p1 + facet_grid(rows = vars(region),  scales="free_x" , as.table=TRUE) + xlab("Year") +
-  ylab("wILI (%)") 
+  ylab("wILI (%)")
 dev.off()
