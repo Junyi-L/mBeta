15b6fd609a222569e7e3823415c5c49dad0b2328
Sebastian Meyer <seb.meyer@fau.de>
2020-08-13 17:57:02 +0200

    more careful argument checking

--- Appl2/forecast_mBeta.R
+++ Appl2/forecast_mBeta.R
@@ -4,7 +4,9 @@ library(data.table)
 library(Formula)
 
 forecast_mBeta <- function(model, newdata, ...){
-  if(class(model) != "betareg") stop("The class of model should be betareg!")
+  if(!inherits(model, "betareg") ||
+     !is_tsibble(model$data))
+      stop("Need betareg model from fit_mBeta() with tsibble data attached)!")
   # Prepare for AR and NB in fit_mBeta.
 
   if(!is_tsibble(newdata)) stop("Newdata must be a tsibble object!")
@@ -12,14 +14,14 @@ forecast_mBeta <- function(model, newdata, ...){
 
   new_index <- index_var(newdata)
   if(is.null(new_index)) stop("The index of newdata must be provided!")
-  
+
   data <- model$data
   AM <- model$AM
   formula <- Formula(model$formula)
-  
+
   key_tsi <- key_vars(data)
   index_tsi <- index_var(data)
-  
+
   if(index_tsi != new_index) stop("The indices of newdata and training data must be the same!")
 
   myterms <- terms(formula, specials = c("AR", "NB"))
@@ -36,7 +38,7 @@ forecast_mBeta <- function(model, newdata, ...){
   # Keep the last several observations in training data for AR and NB.
   ntotal <- nrow(data)
   nkey <- length(unique(data[[key_tsi]]))
-  
+
   nobs <- ntotal/nkey
   unwhich <- function(x, n) {
     out <- rep_len(FALSE, n)
@@ -45,20 +47,20 @@ forecast_mBeta <- function(model, newdata, ...){
   }
   lags_index <- rep(unwhich((nobs - max_lag + 1) : nobs, nobs),
                     times = nkey)
-  
+
   data_lag <- data[lags_index, ]
   train_index <- max(data_lag[[index_tsi]])
   data_lag <- rbind.data.frame(data_lag, newdata)
-  
+
   tsiObj <- as_tsibble(data_lag, key = key_tsi, index = index_tsi, regular = TRUE)
   test_index <- tsiObj[[index_tsi]] > train_index
-  
-  predict_result <- 
+
+  predict_result <-
     predict(model, tsiObj,...)
   #browser()
   res <- predict_result[test_index]
   names(res) <- NULL
-  return(res)  
+  return(res)
 }
 
 
