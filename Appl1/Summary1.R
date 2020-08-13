f5c8ae24f4c5648e707a19abe0b557ef07930d60
Sebastian Meyer <seb.meyer@fau.de>
2020-08-13 16:27:04 +0200

    whitespace

--- Appl1/Summary1.R
+++ Appl1/Summary1.R
@@ -5,12 +5,12 @@ source(file = here::here("./Appl1/Beta.R"))
 load(file = here::here("./Results/sarima_fit.RData"))
 
 model_name <- c(rep("Beta", 5), "SARIMA")
-model_type <- 
+model_type <-
   c("a: full model",
     "b: no trend in $\\phi_t$",
     "c: $\\phi_t = \\phi$",
     "d: $\\beta_k = 0$",
-    "e: $g(x_{t-k}) = x_{t-k}$", 
+    "e: $g(x_{t-k}) = x_{t-k}$",
     "SARIMA")
 # a: whole model
 # b: constant precision
@@ -26,15 +26,15 @@ NNA_idx <- as.logical(SARIMA_idx * Beta_idx)
 # [1] 905
 
 model_logLik <- c(sum(Beta_lags_fit_a$LL[NNA_idx], na.rm = TRUE),
-                  sum(Beta_lags_fit$LL[NNA_idx], na.rm = TRUE), 
+                  sum(Beta_lags_fit$LL[NNA_idx], na.rm = TRUE),
                   sum(Beta_con_p_fit$LL[NNA_idx], na.rm = TRUE),
                   sum(Beta_lags_fit_no_lag$LL[NNA_idx], na.rm = TRUE),
-                  sum(Beta_lags_nologit_fit$LL[NNA_idx], na.rm = TRUE), 
+                  sum(Beta_lags_nologit_fit$LL[NNA_idx], na.rm = TRUE),
                   sum(sarima_fit$LL[NNA_idx], na.rm = TRUE))
 model_n <- nobs(Beta_lags_fit_a)
 model_npar <- c(dim(Beta_lags_fit_a$vcov)[1],
-                dim(Beta_lags_fit$vcov)[1], 
-                dim(Beta_con_p_fit$vcov)[1], 
+                dim(Beta_lags_fit$vcov)[1],
+                dim(Beta_con_p_fit$vcov)[1],
                 dim(Beta_lags_fit_no_lag$vcov)[1],
                 dim(Beta_lags_nologit_fit$vcov)[1],
                 (sarima_fit$aic + 2 * sarima_fit$loglik)/2)
@@ -50,20 +50,20 @@ BIC <- paste0(formatC(round(BIC, digits = 0), format='f', digits=0 ), " (",rank(
 model_logLik <-  paste0(formatC(round(model_logLik, digits = 0), format='f', digits=0 ), " (",rank(- model_logLik), ")")
 #model_npar <- paste0(formatC(round(model_npar, digits = 0), format='f', digits=0 ), "(",rank(model_npar), ")")
 
-summary_table <- data.frame(Model = model_name, 
+summary_table <- data.frame(Model = model_name,
                             model_type,
-                            LogLik = model_logLik, 
+                            LogLik = model_logLik,
                             npar = model_npar,
                             AIC = gsub("-","--",AIC),
-                            AICc = gsub("-","--",AICc), 
+                            AICc = gsub("-","--",AICc),
                             BIC = gsub("-","--",BIC))
 colnames(summary_table) <- c("Model", "Variant", "LL", "npar", "AIC","AICc","BIC")
 
 library(xtable)
 print(xtable(summary_table, align = "ccl|rrrrr",
-             caption = 'Summaries of model fit. 
-             Ranks are shown in bracket. 
-             The loglikelihood (LL) is ranked descending, 
+             caption = 'Summaries of model fit.
+             Ranks are shown in bracket.
+             The loglikelihood (LL) is ranked descending,
              and the AIC, AICc and BIC are ranked ascending.',
              label = "tab:wILIsum",
              digits = 0),
@@ -77,4 +77,3 @@ print(xtable(summary_table, align = "ccl|rrrrr",
       timestamp = NULL
       )
 #saveRDS(summary_table, file = here::here("./Theory_results/flusummary.rds"))
-
