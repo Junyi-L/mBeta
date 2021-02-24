0d78d431618d59feb12975b5198ed27203ede7d2
Sebastian Meyer <seb.meyer@fau.de>
2021-02-24 16:55:58 +0100

    tweak labels

--- Appl1/acf_plot.R
+++ Appl1/acf_plot.R
@@ -7,47 +7,45 @@ pcex <- 0.8
 pline <- -1.5
 ylim1 <- c(-0.15,1)
 pdf(file = here::here("./Plots/ACF.pdf"), height = 4, width = 6)
-layout(mat = matrix(c(1,2,3,4,5,6), 
-                   nrow = 3, 
-                   ncol = 2),
-      heights = c(1, 1, 1.15),    # Heights of the two rows
-      widths = c(1, 1))     # Widths of the two columns
 
-par(mar = c(0,5,0,0))
+par(mfcol = c(3, 2))
+par(oma = c(5, 3, 1, 3))
+
+par(mar = c(0,2,0,0))
 acf(data_a$resid, lag.max = 52, na.action = na.pass,
-    xaxt = "n", ylim = ylim1)
-mtext("Beta a", side = 3, line = pline, cex = pcex)
+    ann = FALSE, xaxt = "n", ylim = ylim1)
+mtext("Beta model (a)", side = 3, line = pline, cex = pcex)
 
 acf(data_c$resid, lag.max = 52,  na.action = na.pass,
-    xaxt = "n", ylim = ylim1)
-mtext("Beta c", side = 3, line= pline, cex = pcex)
-
-par(mar = c(2,5,0,0))
+    ann = FALSE, xaxt = "n", ylim = ylim1)
+mtext("Beta model (c)", side = 3, line= pline, cex = pcex)
 
 acf(data_e$resid, lag.max = 52, main = "Beta e", na.action = na.pass,
-    xaxt = "n", ylim = ylim1)
-mtext("Beta e", side = 3, line= pline, cex = pcex)
+    ann = FALSE, xaxt = "n", ylim = ylim1)
+mtext("Beta model (e)", side = 3, line= pline, cex = pcex)
+
 axis(1)
 
 
-par(mar = c(0,0,0,5))
+par(mar = c(0,0,0,2))
 
 acf(data_b$resid, lag.max = 52, na.action = na.pass,
-    xaxt = "n", yaxt = "n", ylim = ylim1)
-mtext("Beta b", side = 3, line = pline, cex = pcex)
+    ann = FALSE, xaxt = "n", yaxt = "n", ylim = ylim1)
+mtext("Beta model (b)", side = 3, line = pline, cex = pcex)
 axis(4)
 
 acf(data_d$resid, lag.max = 52,  na.action = na.pass,
-    xaxt = "n",yaxt = "n", ylim = ylim1)
-mtext("Beta d", side = 3, line = pline, cex = pcex)
+    ann = FALSE, xaxt = "n",yaxt = "n", ylim = ylim1)
+mtext("Beta model (d)", side = 3, line = pline, cex = pcex)
 axis(4)
 
-
-par(mar = c(2,0,0,5))
 acf(sarima_fit$ARIMA_resid, lag.max = 52, main = "Gaussian c", na.action = na.pass,
-    xaxt = "n",yaxt = "n",
-     ylim = ylim1)
+    ann = FALSE, xaxt = "n",yaxt = "n", ylim = ylim1)
 mtext("SARIMA", side = 3, line= pline, cex = pcex)
 axis(4)
+
 axis(1)
+title(xlab = "Lag", outer = TRUE)
+title(ylab = "ACF", outer = TRUE, line = 1)
+
 dev.off()
